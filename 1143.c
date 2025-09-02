03602128444237de8e1339b7af2be69e0ab8c342
Rojalin swain <110216252+bmsita@users.noreply.github.com>
2025-09-02 23:28:08 +0530

    Create 1143.c

---
+++ 1143.c
@@ -0,0 +1,21 @@
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int longestCommonSubsequence(char* text1, char* text2) {
+    int m = strlen(text1);
+    int n = strlen(text2);
+    int dp[m+1][n+1];
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (text1[i-1] == text2[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
